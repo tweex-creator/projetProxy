e5d8dfabeb18f402e53e7375b2df1f0660cb1fad
Herve <herve.eruam@etu.univ-savoie.fr>
2023-05-20 18:00:00 +0200

    ajout de la mesure du temps de cryptage/decryptage

--- speedTest.py
+++ speedTest.py
@@ -1,13 +1,51 @@
 import time
 import urllib.request
 
-moyenne = 0
-for i in range(0, 10):
-    start = time.time()
-    urllib.request.urlopen("http://httpforever.com/")
-    end = time.time()
-    print(end - start)
-    moyenne += end - start
+from numpy import char
 
-print("Moyenne: ", moyenne/10)
+import our_cryptage
 
+
+def speedTest(url):
+    moyenne = 0
+    for i in range(0, 10):
+        start = time.time()
+        urllib.request.urlopen(url)
+        end = time.time()
+        print(end - start)
+        moyenne += end - start
+
+    print("Moyenne " + url + " : ", moyenne / 10)
+
+
+def cryptageSpeedTest():
+    # Donne le temps nescessaire pour crypter des messages de 10, 100, 1000 et 10000 caractères
+    our_cryptage.setConnectionState(2)
+    our_cryptage.setSymetricKey(our_cryptage.getNewSymetricKey())
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    base = 10000000
+    message = ""
+    for j in range(0, base):
+        message += alphabet[j % 26]
+
+    for i in range(1, 50):
+        message_loc = ""
+        for j in range(0, i):
+            message_loc += message
+
+        message_loc = message_loc.encode()
+        start = time.time()
+        res = our_cryptage.cryptage(message_loc)
+        end = time.time()
+        time_crypt  =
+        print("Temps pour crypter un message de " + str(len(message_loc)) + " (" + str(len(res)) + ") caractères : ", (end - start) * 1000, " milli secondes")
+        start = time.time()
+        res = our_cryptage.decryptage(res)
+        end = time.time()
+        print("Temps pour décrypter un message de " + str(len(res)) + " (" + str(len(res)) + ") caractères : ", (end - start) * 1000, " milli secondes")
+        print("")
+        #Export les resultat dans excel
+
+
+cryptageSpeedTest()
